923f220a10647b1e88d8e605ad68889ad8477d31
Mystery406 <17538405+Mystery406@users.noreply.github.com>
2026-08-22 23:44:01 +0800

    Configure desktop packaging modules and Windows shortcut

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -19,11 +19,13 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            modules("jdk.unsupported")
             packageName = "Minesweeper"
             packageVersion = "1.0.0"
 
             windows {
                 iconFile.set(project.file("src/main/resources/app-icon.ico"))
+                shortcut = true
             }
             linux {
                 iconFile.set(project.file("src/main/composeResources/drawable/app_icon.png"))
